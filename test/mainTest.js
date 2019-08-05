25bd82b34b424b2910b1c9cc2a73475bca141397
José Ángel Sánchez (j0n3) <jone79@gmail.com>
2019-08-05 17:46:42 +0200

    Add basic test to check set up

---
+++ test/mainTest.js
@@ -0,0 +1,7 @@
+var assert = require('assert')
+describe('mocha testing is working', function ()
+    {
+        it('assert true is true', function () {
+            assert.equal(true, true);
+        });
+    });
